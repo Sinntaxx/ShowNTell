e6ff32336a695932c09f07bf4e1a96b443dd47e5
Logan-Ward <ward_logan@outlook.com>
2022-09-21 15:54:46 -0500

    feat: welcome message upon subscribing to Game&Watch

--- server/Routes/game.js
+++ server/Routes/game.js
@@ -11,19 +11,37 @@ game.get('/test', (req, res) => {
 
 // url to notify webpage that a user has started a chat with the bot: https://${siteUrl}/game/newUser
 game.post('/newUser', (req, res) => {
-  console.log('arrived');
-  console.log(req.body);
-  res.sendStatus(200);
+  const { message } = req.body;
+  if (message.text.split(' ')[0] === '/start') {
+    // This is the user token to associate the user in the database with the user on telegram
+    const userToken = message.text.split(' ')[1];
+    const chatId = message.chat.id;
+    axios
+      .post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+        chat_id: chatId,
+        text: 'You are now subscribed to notifications from Game&Watch',
+      })
+      .then((result) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(400);
+      });
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 // url to have user click: https://telegram.me/GameAndWatchBot?start=${userId}
 game.post('/notify', (req, res) => {
   const { name, updateTitle, link, chat_id, number } = req.body;
   const text = `**New Update**\n${name}\n${updateTitle}\n${link}`;
-  axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-    chat_id,
-    text,
-  })
+  axios
+    .post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+      chat_id,
+      text,
+    })
     .then((result) => {
       console.log(result);
       res.sendStatus(200);

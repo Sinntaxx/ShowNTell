dc71686a84b6c2d5d2194c78e97eaf79d318d33d
Logan-Ward <ward_logan@outlook.com>
2022-09-21 10:20:43 -0500

    feat: created telegram bot

--- server/Routes/game.js
+++ server/Routes/game.js
@@ -1,10 +1,36 @@
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
 const express = require('express');
+const axios = require('axios');
 
 const game = express.Router();
 
 // example
 game.get('/test', (req, res) => {
   res.sendStatus(200);
+});
+
+// url to notify webpage that a user has started a chat with the bot: https://${siteUrl}/game/newUser
+game.post('/newUser', (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+// url to have user click: https://telegram.me/GameAndWatchBot?start=${userId}
+game.post('/notify', (req, res) => {
+  const { name, updateTitle, link, chat_id, number } = req.body;
+  const text = `**New Update**\n${name}\n${updateTitle}\n${link}`;
+  axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    chat_id,
+    text,
+  })
+    .then((result) => {
+      console.log(result);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(400);
+    });
 });
 
 module.exports = game;

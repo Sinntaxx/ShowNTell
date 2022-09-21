8b03ce9e81ce969122d41d77e4cdc806408a1846
LoganVoisin <flyflameking@gmail.com>
2022-09-21 12:01:34 -0500

    add: basic request to the steam api

--- server/Routes/game.js
+++ server/Routes/game.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const axios = require('axios');
 
 const game = express.Router();
 
 // example
 game.get('/test', (req, res) => {
   res.sendStatus(200);
+});
+
+// this endpoint will get games by a genre and take return the first 10.
+game.get('/genre', (req, res) => {
+  const config = {
+    method: 'get',
+    url: 'http://steamspy.com/api.php?request=genre&genre=Action&page=1',
+    headers: { },
+  };
+
+  axios(config)
+    .then((response) => {
+      res.status(200).send(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 module.exports = game;

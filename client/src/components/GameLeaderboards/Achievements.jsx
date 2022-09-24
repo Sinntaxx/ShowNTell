8fb6fed6cfce571be79da4409d42cf7b2f68e504
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-09-24 01:50:02 -0500

    feat: batch axios request in use effect working

--- client/src/components/GameLeaderboards/Achievements.jsx
+++ client/src/components/GameLeaderboards/Achievements.jsx
@@ -2,10 +2,37 @@ import React, { useState, useEffect } from 'react';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListSubheader from '@mui/material/ListSubheader';
+import axios from 'axios';
 
 import AchievementEntry from './AchievementEntry.jsx';
 
-const Achievements = () => {
+const Achievements = ({ user }) => {
+  // select game to display achievements for
+  const [game, setGame] = useState({});
+  const [gameList, setGameList] = useState([]);
+
+  // get users game subscriptions
+  useEffect(() => {
+    axios.get(`/game/subscriptions/${user.id}`)
+      .then(({ data }) => {
+        console.log('users gameSubs from db', data);
+        const endPoints = data.map((id) => {
+          return `/game/byId/${id}`;
+        });
+        // console.log('endpoints to get each game\n', endPoints);
+        return axios.all(endPoints.map((endPoint) => axios.get(endPoint)));
+      })
+      .catch((err) => {
+        console.error('error on retrieving gameSubs in client axios\n', err);
+      })
+      .then(axios.spread((...res) => {
+        console.log('a single response from axios.all\n', res[2].data);
+      }))
+      .catch((err) => {
+        console.error('error on batch request\n', err);
+      });
+  }, []);
+
   return (
     <List
       sx={{

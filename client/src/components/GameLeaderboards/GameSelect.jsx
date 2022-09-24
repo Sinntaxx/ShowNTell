b445386858bc1bc15f2f23de18d08721634ad0c3
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-09-24 12:42:45 -0500

    feat: component for dropdown options with autofill

---
+++ client/src/components/GameLeaderboards/GameSelect.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+// material ui
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Achievements from './Achievements.jsx';
+
+const GameSelect = () => {
+  return (
+    <div />
+  );
+};
+
+export default GameSelect;
